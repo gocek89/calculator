0d4d1244a2ef1bd9b50c047c9da6a4d2c7ebbd5d
Gocek <gocek89@gmail.com>
2021-12-06 12:31:17 +0100

    main

--- calculator.py
+++ calculator.py
@@ -6,57 +6,51 @@ def adding(numb_1, numb_2, numb_3=0, numb_4=0):
     x = str(input(
         "W wypadku tego działania mozesz dodac do siebie jeszcze dwie cyfry czy chcesz skorzystac z mozliwosci [T/N]"))
     if x == "T":
-        numb_3 = int(input("Podaj liczbę 3:\n"))
-        numb_4 = int(input("Podaj liczbę 4:\n"))
+        numb_3 = int(input("Podaj liczbę 3: "))
+        numb_4 = int(input("Podaj liczbę 4: "))
         logging.info(
             f"Dodaje składnik {numb_1} i {numb_2} i {numb_3} i {numb_4}")
-        wynik = numb_1 + numb_2 + numb_3 + numb_4
-        print(f"Wynik to : {wynik}")
+        return numb_1 + numb_2 + numb_3 + numb_4
     else:
         logging.info(f"Dodaje składnik {numb_1} i {numb_2}")
-        wynik = numb_1 + numb_2
-        print(f"Wynik to : {wynik}")
+        return numb_1 + numb_2
 
 
 def subtraction(numb_1, numb_2):
     logging.info(f"odejmuję {numb_1} i {numb_2}")
-    wynik = numb_1 - numb_2
-    print(f"Wynik to : {wynik}")
+    return numb_1 - numb_2
 
 
 def multiplication(numb_1, numb_2, numb_3=1, numb_4=1):
     x = str(input(
         "W wypadku tego działania mozesz dodac do siebie jeszcze dwie cyfry czy chcesz skorzystac z mozliwosci [T/N]"))
     if x == "T":
-        numb_3 = int(input("Podaj liczbę 3:\n"))
-        numb_4 = int(input("Podaj liczbę 4:\n"))
-        wynik = numb_1 * numb_2 * numb_3 * numb_4
+        numb_3 = int(input("Podaj liczbę 3: "))
+        numb_4 = int(input("Podaj liczbę 4: "))
         logging.info(
             f"Mnoze składnik {numb_1} i {numb_2} i {numb_3} i {numb_4}")
-        print(f"Wynik to : {wynik}")
+        return numb_1 * numb_2 * numb_3 * numb_4
     else:
-        wynik = numb_1 * numb_2
         logging.info(f"Mnoze składniki{numb_1} i {numb_2}")
-        print(f"Wynik to : {wynik}")
+        return numb_1 * numb_2
 
 
 def division(numb_1, numb_2):
     logging.info(f"dziele {numb_1} i {numb_2}")
-    wynik = numb_1 / numb_2
-    print(f"Wynik to : {wynik}")
+    return numb_1 / numb_2
 
 
 choice = int(input(
-    "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n"))
+    "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: \n"))
 
 numb_1 = int(input("Podaj liczbę 1: "))
 numb_2 = int(input("Podaj liczbę 2: "))
 
 if choice == 1:
-    wynik = adding(numb_1, numb_2)
+    print(adding(numb_1, numb_2))
 elif choice == 2:
-    wynik = subtraction(numb_1, numb_2)
+    print(subtraction(numb_1, numb_2))
 elif choice == 3:
-    wynik = multiplication(numb_1, numb_2)
+    print(multiplication(numb_1, numb_2))
 elif choice == 4:
-    wynik = division(numb_1, numb_2)
+    print(division(numb_1, numb_2))
